6bcab07802aa29bbb7f0231b1d52149159b117ac
arakawahokuto <4869bigdipper@gmail.com>
2022-03-03 17:09:55 +0900

    コミット

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,50 @@ $(function () {
         easing: "swing"
     });
 
+    // -----------------------------スクロール時にフェードインさせたいアイテム-----------------------------
+
+    $(window).on("scroll",function(){
+
+        // ---現在地を取ってくる---
+        let now = $(window).scrollTop();
+
+        // ---これだと(.js-fadein)のクラスを持ってる一番上の要素に反応してしまう。---
+        // ---↓↓このままだとスクロールした時に一番上の高さで反応してしまってそれぞれの高さでフェードインしない---
+        // let item_h = $(".js-fadein").offset().top;
+
+        // ---画面の高さを取ってくる---
+        let windowheight = $(window).height();
+
+        // ---(.js-fadein)のクラスを持っている要素全てを変数targetsに入れる---
+        let targets = $(".js-fadein");
+
+        // ---targetsに対してeach()を使ってindex(インデックス番号)とelement(HTML要素)を取得する---
+        targets.each(function(index,element){
+
+        // ---変数item_hに(.js-fadein)を含むそれぞれの高さを入れる---
+        let item_h = $(element).offset().top;
+
+        // now + windowheight で画面下の高さを取ってきて変数item_hが画面に出てきた時の条件をつける---
+        if(now + windowheight > item_h){
+
+            // (.js-fadein)にアニメーションの動きの(.fadein-item)をつける---
+            $(".js-fadein").addClass("fadein-item");
+        }
+        })
+    })
+
+    // ---------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
     // let windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
     // if(windowwidth > 768){
     //     let responsiveImage = [
