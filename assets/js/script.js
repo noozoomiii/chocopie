6d54746b10e6662a70b989a858b360702dbf1914
arakawahokuto <4869bigdipper@gmail.com>
2022-03-07 09:54:17 +0900

    コミット

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,11 @@ $(function () {
         duration: 750,
         easing: "swing"
     });
+    // ------------------------------------------------------------------------------------------------
+
+
 
     // -----------------------------スクロール時にフェードインさせたいアイテム-----------------------------
-
     $(window).on("scroll",function(){
 
         // ---現在地を取ってくる---
@@ -44,96 +46,65 @@ $(function () {
 
         // ---targetsに対してeach()を使ってindex(インデックス番号)とelement(HTML要素)を取得する---
         targets.each(function(index,element){
-
         // ---変数item_hに(.js-fadein)を含むそれぞれの高さを入れる---
         let item_h = $(element).offset().top;
-
         // now + windowheight で画面下の高さを取ってきて変数item_hが画面に出てきた時の条件をつける---
         if(now + windowheight > item_h){
-
             // $(element)※それぞれの高さを取って※アニメーションの動きの(.fadein-item)をつける---
             $(element).addClass("fadein-item");
         }
         })
     })
-
     // ---------------------------------------------------------------------------------------------
 
-    // $(".toggle_btn").on("click",function(){
-    //     // 現在地を取る---
-    //     let jump_now = $(window).scrollTop();
-    //     // 画面の高さを取る---
-    //     let jump_window = $(window).height();
-    //     // 画面下の位置を取る---
-    //     let jump_bottom = jump_now + jump_window;
-    //     // #naviの位置を取る---
-    //     let jump_navi = $("#navi").offset().top;
-        
-    //     if(jump_navi == -573.40625){
-    //         $("#navi").addClass("jump-down");
-    //     }
-    // })
+    
+
+    // ---------------------------トグルの動き------------------------------
+    // ---何回押したかカウントする為箱を用意する---
     let count = 0;
+
+    // ---クリックした時に1ずつ足していく---
     $(".toggle_btn").on("click",function(){
             count = count + 1;
+            // ---奇数の時の処理---
             if(count % 2 == 1){
                 $("#navi").removeClass("jump-up2");
                 $("#navi").addClass("jump-up");
+            // ---偶数の時の処理---
             }else{
                 $("#navi").removeClass("jump-up");
                 $("#navi").addClass("jump-up2");
             }
     })
+    // -------------------------------------------------------------------
 
-
-    
-
-    // $(".toggle_btn").on("click",function(){
-    //     // 現在地を取る---
-    //     let jump_now = $(window).scrollTop();
-    //     // 画面の高さを取る---
-    //     let jump_window = $(window).height();
-    //     // 画面下の位置を取る---
-    //     let jump_bottom = jump_now + jump_window;
-    //     // #naviの位置を取る---
-    //     let jump_navi = $("#navi").offset().top;
-        
-    //     if(jump_navi == 0){
-    //         $("#navi").addClass("jump-up");
-    //     }
-    // })
 
 
     // -------------------------トグルメニュー--------------------------------
-
     $(".toggle_btn").on("click",function(){
 
         // ---トグルをクリックした時に#headerが.openを持っていたら---
             // ---オープンを外す(閉じる)---    
         if($("#header").hasClass("open")){
             $("#header").removeClass("open");
-
             // ---オープンを付ける(開ける)---
         }else{
             $("#header").addClass("open");
         }
     })
-
     // --------------------------------------------------------------------
 
 
 
-    // ---------トグルのリンクを押したらトグルを閉める------------
-
+    // -------------------トグルのリンクを押したらトグルを閉める-----------------------
     $("#navi a").on("click",function(){
         $("#header").removeClass("open");
     });
+    // --------------------------------------------------------------------------
 
-    // -----------------------------------------------------
 
 
     // ---------------------1文字ずつ出てくるアニメーション-------------------------
-
     // ---アニメーションさせるクラス---
     let container = $(".bg-title");
 
@@ -160,7 +131,6 @@ $(function () {
         container.find("span").eq(txtNum).css({opacity: 1});
         txtNum++
     },speed);
-
     // -------------------------------------------------------------------------
 
     
